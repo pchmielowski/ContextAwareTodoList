365e6e14f673dbf8aea599cb7cfbb6d6944b801b
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-04-03 16:21:23 +0200

    First test for SqlTask after rfactoring

--- app/src/test/java/com/chmielowski/contexttasklist/SqlTaskListTest.java
+++ app/src/test/java/com/chmielowski/contexttasklist/SqlTaskListTest.java
@@ -1,55 +1,53 @@
 package com.chmielowski.contexttasklist;
 
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class SqlTaskListTest {
 //
-//    @Mock
-//    TaskListView view;
-//
-//    @Mock
-//    Persistence dataBase;
-//
-//    @Test
-//    public void showOn_emptyDataBase_callsGetTasks() {
-//        // arrange
-//        when(dataBase.getTasks()).thenReturn(new Iterable<Task>() {
-//            @Override
-//            public Iterator<Task> iterator() {
-//                return new ArrayList<Task>().iterator();
-//            }
-//        });
-//        SqlTaskList taskList = new SqlTaskList(dataBase);
-//
-//        // act
-//        taskList.showOn(view);
-//
-//        // assert
-//        verify(dataBase).getTasks();
-//    }
-//
-//    @Mock
-//    Task task;
-//
-//    @Test
-//    public void showOn_dataBaseOneTask_callsShowOn() {
-//        // arrange
-//        when(dataBase.getTasks()).thenReturn(new Iterable<Task>() {
-//            @Override
-//            public Iterator<Task> iterator() {
-//                ArrayList<Task> list = new ArrayList<>();
-//                list.add(task);
-//                return list.iterator();
-//            }
-//        });
-//        SqlTaskList taskList = new SqlTaskList(dataBase);
-//
-//        // act
-//        taskList.showOn(view);
-//
-//        // assert
-//        verify(task).showOn(view);
-//    }
-}
+//@RunWith(MockitoJUnitRunner.class)
+//public class SqlTaskListTest {
+////
+////    @Mock
+////    TaskListView view;
+////
+////    @Mock
+////    Persistence dataBase;
+////
+////    @Test
+////    public void showOn_emptyDataBase_callsGetTasks() {
+////        // arrange
+////        when(dataBase.getTasks()).thenReturn(new Iterable<Task>() {
+////            @Override
+////            public Iterator<Task> iterator() {
+////                return new ArrayList<Task>().iterator();
+////            }
+////        });
+////        SqlTaskList taskList = new SqlTaskList(dataBase);
+////
+////        // act
+////        taskList.showOn(view);
+////
+////        // assert
+////        verify(dataBase).getTasks();
+////    }
+////
+////    @Mock
+////    Task task;
+////
+////    @Test
+////    public void showOn_dataBaseOneTask_callsShowOn() {
+////        // arrange
+////        when(dataBase.getTasks()).thenReturn(new Iterable<Task>() {
+////            @Override
+////            public Iterator<Task> iterator() {
+////                ArrayList<Task> list = new ArrayList<>();
+////                list.add(task);
+////                return list.iterator();
+////            }
+////        });
+////        SqlTaskList taskList = new SqlTaskList(dataBase);
+////
+////        // act
+////        taskList.showOn(view);
+////
+////        // assert
+////        verify(task).showOn(view);
+////    }
+//}
--- app/src/test/java/com/chmielowski/contexttasklist/SqlTaskTest.java
+++ app/src/test/java/com/chmielowski/contexttasklist/SqlTaskTest.java
@@ -1,34 +1,33 @@
 package com.chmielowski.contexttasklist;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 
 public class SqlTaskTest {
-//
-//    @Mock
-//    TaskListView view;
-//
-//    @Test
-//    public void showOn_withView_callsShowTask() {
-//        // arrange
-//        SqlTask task = new SqlTask(false, "task name");
-//
-//        // act
-//        task.showOn(view);
-//
-//        // assert
-//        verify(view).showTask(false, "task name");
-//    }
-//
-//    // TODO: change exception type
-//    @Test(expected=NullPointerException.class)
-//    public void showOn_withNull_NpeThrown() {
-//        // arrange
-//        SqlTask task = new SqlTask(false, "task name");
-//
-//        // act & assert
-//        task.showOn(null);
-//    }
+
+    @Mock
+    TaskListView view;
+
+    @Mock
+    Persistence dataBase;
+
+    @Test
+    public void showsValidTaskName() throws Exception {
+        // arrange
+        when(dataBase.string("Name", "id = 123")).thenReturn("testing");
+        SqlTask task = new SqlTask(123, dataBase);
+
+        // act
+        task.showOn(view);
+
+        // assert
+        verify(view).showTask(true, "testing");
+    }
 }
