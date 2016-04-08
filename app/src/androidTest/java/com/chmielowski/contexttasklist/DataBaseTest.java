258aa645a38186b5c69b650cbb43c1a40201e4f2
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-04-08 18:48:04 +0200

    Instrumentation test configuration

---
+++ app/src/androidTest/java/com/chmielowski/contexttasklist/DataBaseTest.java
@@ -0,0 +1,14 @@
+package com.chmielowski.contexttasklist;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+
+@SmallTest
+public class DataBaseTest {
+
+    @Test
+    public void empty() throws Exception {
+
+    }
+}
