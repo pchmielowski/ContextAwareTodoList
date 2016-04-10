f218f48239553e63b8268090e3874a6849c9b075
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-04-10 19:14:58 +0200

    DataBaseTest - first two

--- app/src/androidTest/java/com/chmielowski/contexttasklist/DataBaseTest.java
+++ app/src/androidTest/java/com/chmielowski/contexttasklist/DataBaseTest.java
@@ -7,12 +7,34 @@ import android.test.suitebuilder.annotation.SmallTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public class DataBaseTest extends AndroidTestCase {
 
     @Test
-    public void testAppTitle() {
-        assertEquals("Context Tasklist", getContext().getResources().getString(R.string.app_name));
+    public void correctIntegers() {
+        DataBase dataBase = new DataBase(getContext());
+
+        try {
+            List<Integer> result = dataBase.integers("id", "");
+            assertEquals(Arrays.asList(1, 2), result);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void correctString() {
+        DataBase dataBase = new DataBase(getContext());
+
+        try {
+            String result = dataBase.string("name", "id = 1");
+            assertEquals("programming", result);
+        } catch (Exception e) {
+            fail();
+        }
     }
 }
