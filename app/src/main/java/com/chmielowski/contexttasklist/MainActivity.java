ac8a8f86f2041412e7f584b97f4b57786983a1c2
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-04-11 18:26:08 +0200

    Add Task button and dummy Add Task dialog box

--- app/src/main/java/com/chmielowski/contexttasklist/MainActivity.java
+++ app/src/main/java/com/chmielowski/contexttasklist/MainActivity.java
@@ -1,21 +1,55 @@
 package com.chmielowski.contexttasklist;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements TaskListView {
+    private String m_Text;
+
     @Override
     protected final void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Persistence dataBase = new DataBase(this, "Tasks");
+        final TaskList taskList = new SqlTaskList(dataBase);
         try {
-            new SqlTaskList(new DataBase(this, "Tasks")).showOn(this);
+            taskList.showOn(this);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add new task");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog dialog = builder.create();
+        Button addTaskButton = (Button) findViewById(R.id.add_task_button);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                dialog.show();
+            }
+        });
     }
 
     @Override
