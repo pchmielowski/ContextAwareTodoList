50ed94dad9b64c43b5ed952c63ea6b5ea105a416
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-04-11 16:36:54 +0200

    DataBaseTest added

---
+++ app/src/androidTest/java/com/chmielowski/contexttasklist/DataBaseTest.java
@@ -0,0 +1,76 @@
+package com.chmielowski.contexttasklist;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RunWith(AndroidJUnit4.class)
+public class DataBaseTest extends AndroidTestCase {
+
+    private static final String TABLE_NAME = "test";
+
+    @Before
+    public void setUp() {
+        SQLiteOpenHelper sql = new SQLiteOpenHelper(getContext(), "tasks.db", null, 1) {
+            @Override
+            public void onCreate(SQLiteDatabase db) {
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            }
+        };
+        sql.getWritableDatabase().execSQL("DROP TABLE IF EXISTS " + TABLE_NAME + ";");
+        sql.getWritableDatabase().execSQL("CREATE TABLE IF NOT EXISTS " +
+                TABLE_NAME +
+                " (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "strings TEXT, " +
+                "booleans INTEGER" +
+                ");");
+        sql.getWritableDatabase().execSQL("INSERT INTO " + TABLE_NAME
+                + " (strings, booleans) VALUES ('first', 0);");
+        sql.getWritableDatabase().execSQL("INSERT INTO " + TABLE_NAME
+                + " (strings, booleans) VALUES ('second', 1);");
+    }
+
+    @MediumTest
+    public void returns_correct_integers() throws Exception {
+        DataBase dataBase = new DataBase(getContext(), TABLE_NAME);
+
+        List<Integer> result = dataBase.integers("id", "");
+
+        assertEquals(Arrays.asList(1, 2), result);
+    }
+
+    @MediumTest
+    public void returns_correct_strings() throws Exception {
+        DataBase dataBase = new DataBase(getContext(), TABLE_NAME);
+
+        String result1 = dataBase.string("strings", "id = 1");
+        String result2 = dataBase.string("strings", "id = 2");
+
+        assertEquals("first", result1);
+        assertEquals("second", result2);
+    }
+
+    @MediumTest
+    public void returns_correct_booleans() throws Exception {
+        DataBase dataBase = new DataBase(getContext(), TABLE_NAME);
+
+        boolean result1 = dataBase.bool("booleans", "id = 1");
+        boolean result2 = dataBase.bool("booleans", "id = 2");
+
+        assertEquals(false, result1);
+        assertEquals(true, result2);
+    }
+}
