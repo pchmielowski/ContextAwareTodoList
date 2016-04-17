8d0b564142ac2f9dfa36a4d6194b28c4c0b7e6a8
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-04-17 12:32:03 +0200

    TaskList is a fragment

--- app/src/main/java/com/chmielowski/contexttasklist/MainActivity.java
+++ app/src/main/java/com/chmielowski/contexttasklist/MainActivity.java
@@ -1,16 +1,7 @@
 package com.chmielowski.contexttasklist;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-
-import com.chmielowski.contexttasklist.sql.DataBase;
-import com.chmielowski.contexttasklist.sql.SqlTaskList;
-import com.chmielowski.contexttasklist.view.AddTaskDialog;
-import com.chmielowski.contexttasklist.view.TaskListView;
-import com.chmielowski.contexttasklist.view.TaskListViewImpl;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -18,20 +9,5 @@ public class MainActivity extends AppCompatActivity{
     protected final void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Persistence dataBase = new DataBase(this, "Tasks");
-        final TaskList taskList = new SqlTaskList(dataBase);
-        final TaskListView taskListView = new TaskListViewImpl(this);
-        try {
-            taskList.showOn(taskListView);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        final AddTaskDialog dialog = new AddTaskDialog((Context)this, taskListView, taskList);
-        Button addTaskButton = (Button) findViewById(R.id.add_task_button);
-        addTaskButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(final View v) {
-                dialog.show();
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/chmielowski/contexttasklist/view/TaskViewFragment.java
@@ -0,0 +1,91 @@
+package com.chmielowski.contexttasklist.view;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.chmielowski.contexttasklist.Persistence;
+import com.chmielowski.contexttasklist.R;
+import com.chmielowski.contexttasklist.TaskList;
+import com.chmielowski.contexttasklist.commands.ChangeStatusCommand;
+import com.chmielowski.contexttasklist.commands.DeleteTaskCommand;
+import com.chmielowski.contexttasklist.sql.DataBase;
+import com.chmielowski.contexttasklist.sql.SqlTaskList;
+
+public class TaskViewFragment extends Fragment implements TaskListView {
+
+    private View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        v = inflater.inflate(R.layout.task_list, container, false);
+        Persistence dataBase = new DataBase(v.getContext(), "Tasks");
+        final TaskList taskList = new SqlTaskList(dataBase);
+        try {
+            taskList.showOn(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        final AddTaskDialog dialog = new AddTaskDialog(v.getContext(), this, taskList);
+        Button addTaskButton = (Button) v.findViewById(R.id.add_task_button);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                dialog.show();
+            }
+        });
+        return v;
+    }
+
+
+    @Override
+    public final void showTask(final boolean isDone,
+                               final String description,
+                               final ChangeStatusCommand statusCommand,
+                               final DeleteTaskCommand deleteCommand) {
+        int checkboxViewId = View.generateViewId();
+        TaskListView taskListView = (TaskListView) this;
+        taskListLayout().addView(
+                new TaskCheckBox(
+                        isDone,
+                        description,
+                        statusCommand,
+                        new RemoveTaskDialog(
+                                this.v.getContext(),
+                                taskListView,
+                                deleteCommand,
+                                checkboxViewId),
+                        checkboxViewId,
+                        this.v.getContext()
+                ).checkBox()
+        );
+    }
+
+    private LinearLayout taskListLayout() {
+            return (LinearLayout) v.findViewById(R.id.layout_tasks);
+    }
+
+    @Override
+    public final void removeTask(final int id) {
+        try {
+            taskListLayout().removeView(v.findViewById(id));
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public final void clean() {
+        taskListLayout().removeAllViews();
+    }
+
+}
