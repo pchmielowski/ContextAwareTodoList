976960d9cf57ee0864c9853ffb2dbbd4f30aa2c9
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-05-27 22:02:11 +0200

    ChangeTaskStatusCommand test in Kotlin

---
+++ app/src/test/kotlin/com/chmielowski/contexttasklist/commands/ChangeTaskStatusCommandTest.kt
@@ -0,0 +1,20 @@
+package com.chmielowski.contexttasklist.commands;
+
+import com.chmielowski.contexttasklist.Task
+import org.junit.Test
+import org.mockito.Mockito
+
+import org.mockito.Mockito.verify
+
+class ChangeTaskStatusCommandTest {
+
+    @Test
+    fun calls_task_status_true() {
+        val mockedTask = Mockito.mock(Task::class.java)
+        val command = ChangeTaskStatusCommand(mockedTask)
+
+        command.execute(true)
+
+        verify(mockedTask).status(true)
+    }
+}
